a048e13d8fa27f59f0952b9df9217c3f07b6e189
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-06-18 18:52:09 +0200

    removed EsLint warning

--- src/Spinner.js
+++ src/Spinner.js
@@ -3,7 +3,6 @@ import spinner from "./833.gif";
 export default function Spinner() {
   return (
     <div class="lds-roller">
-      // eslint-disable-next-line jsx-a11y/alt-text
       <img src={spinner} />
     </div>
   );
